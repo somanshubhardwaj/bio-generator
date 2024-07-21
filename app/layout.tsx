456c7680d3dfbaa573bc4560d05022b8d1407ab4
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-07-21 23:19:54 +0530

    added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import GridPattern from "@/components/magicui/grid-pattern";
@@ -25,6 +26,7 @@ export default function RootLayout({
         <TooltipProvider>
           {children}
           <Analytics />
+          <SpeedInsights />
         </TooltipProvider>
       </body>
     </html>
